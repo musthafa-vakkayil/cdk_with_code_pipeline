b814e45142a559ef10d75f339e65f439e8a00c3f
Muhammed Musthafa Shahal V <t-m.shahal.v@heart.org>
2025-02-10 16:22:26 +0530

    fix: source directory

--- cdk/lib/hb-technical-stack.ts
+++ cdk/lib/hb-technical-stack.ts
@@ -23,7 +23,7 @@ export class HBTechnicalSupportServcieStack extends cdk.Stack {
 
     new lambda.Function(this, "mylambda", {
       functionName: 'first-cdk-lambda',
-      code: lambda.Code.fromBucket(bucket, `${bucketKey.valueAsString}/lambda.zip`),
+      code: lambda.Code.fromBucket(bucket, bucketKey.valueAsString),
       handler: 'dist/index.handler',
       runtime: lambda.Runtime.NODEJS_20_X,
       memorySize: 128
